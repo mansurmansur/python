ac97d10f49e3fdda04924ded8410047bff9c5766
mansurmansur <mansurmansur@trentu.ca>
2020-06-06 21:48:59 -0400

    The final changes have been made: Added useinput_validator() function: it checks if user entered a number and returns True if a number else false Modified also play() function: major steps in play function are; invoke generate_number function, invoke userinput_validator function to validate user input and then check if the user correctly guessed That is all by Mansur Mansur time of modification: 9:48pm saturday june 6th, 2020.

--- number_guessing.py
+++ number_guessing.py
@@ -18,24 +18,106 @@ def generate_number():
     minimum = 1
     maximum = 50
     
-    return randrange(minimum,maximum)
+    num = random.randint(minimum,maximum)
+    return num
 
 
+# -------------------------------------------------------------------------------- #
+# function name: userinput_validator()
+# parameters : userinput
+# return: True or False
+# -------------------------------------------------------------------------------- #
+def userinput_validator(num):
+    try:
+      val = int(num)
+      return True
+    except ValueError:
+         return False
+         
 # -------------------------------------------------------------------------------- #
 # function name: check_number_guessed()
 # parameters : user guess and number generated
-# return: string
+# return: True or False
 # -------------------------------------------------------------------------------- #
-def check_number_guessed():
+def check_number_guessed(number, guess):
+    #check if user guessed correctly
+    if number == guess:
+       return True
+    elif number > guess:
+       print("Number you entered is smaller")
+       return False
+    elif number < guess:
+       print("Number you entered is larger")
+       return False
 
 # -------------------------------------------------------------------------------- #
-# function name: user_input()
+# function name: play()
 # parameters : void
 # return: none
 # -------------------------------------------------------------------------------- #
-def user_input():
+def play():
+
+  #this terminates the infinite loop when the attemp is 3 times
+  number_of_guess = 0
+ 
+  
+  #generate the number
+  number = generate_number()
+  
+  while True:
+     #terminate the loop if the number of trial is 3
+     if number_of_guess == 3:
+        print("You  lost the game after 3 trials")
+        print("The correct numebr is: {}".format(number))
+        break
+        
+     
+     #print out * for the number randomly generated
+     if number > 10:
+        print("The number to be guessed is: **")
+     else:
+        print("The number to be guessed is: *")
+        
+    
+     #user input
+     print("The number is between 1 and 50")
+     print("You have {} attempts to guess the number".format(3-number_of_guess))
+     guess = input("Enter the number: ")
+     number_of_guess += 1
+     
+     #invoke the validator function
+     while True:
+        if userinput_validator(guess) is True:
+           try:
+             val = int(guess)
+           except ValueError:
+             val = float(gues)
+             
+           if val >= 1 or val <= 50:
+              break
+        
+        print("INVALID INPUT")
+        guess = input("Enter valid number : ")
+     
+     #check if the guess is correct
+     if check_number_guessed(number, val) is False and number_of_guess != 3:
+        print("Please Try again")
+        print("You only have {} more try".format(3-number_of_guess))
+     elif check_number_guessed(number, val) is True:
+        print("You have guessed the correct number which is : {}".format(number))
+        print("After {} try".format(number_of_guess))
+        break
+     
+
+
+# -------------------------------------------------------------------------------- #
+# function name: main()
+# -------------------------------------------------------------------------------- #
+def main():
+    #varibles
     a = "1"
     b = "2"
+    
     #infinite loop
     while True:
        print("Welcome to Number Guessing program")
@@ -53,16 +135,15 @@ def user_input():
           choice = input()
           
        if choice == a:
-       
-          numeber = generate_number()
+          #play
+          play()
           
-          #ask user to guess a number
-          print("Guess the number choosen by the computer between 1 and 50")
-          guess = input()
-          
-          #check if user got it right
-          value = check_number_guessed(numeber, guess)
-          
-          #
        elif choice == b:
+          print("See ya !!!")
+          print("End of the game")
           break
+
+
+# ---- invoke the main function ----------#
+if __name__ == '__main__':
+   main()
