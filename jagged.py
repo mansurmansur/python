ec420371143777587cab137415232af006d5205f
mansurmansur <mansurmansur@trentu.ca>
2020-06-11 20:59:15 -0400

    Modified by Mansur Mansur at june 11th, 2020 at 8:59pm

--- jagged.py
+++ jagged.py
@@ -1,8 +1,25 @@
-# Assignment 3, Question 1
-# Author: your name here
-# McGill ID: your ID here
+'''
+  author: Mansur Mansur
+  
+  Description: This program makes jagged list. a jagged list is list of lists in python that contains lists of different sizes.
+  
+  functions: 2 functions
+        1. make_jagged(data)
+        2. sort_jagged(data)
+        
+  date: june 11th, 2020
+'''
 
 def make_jagged(data):
+    '''
+       This function removes all zeros from each inner lists and all inner lists should contain only integer values. the function do not return any value.
+       
+       algorithms:
+              I.Traverse through the list of lists and convert all elements in each list to integer if there is error throw an exception and exit the program.
+              II. Traverse through the Lists of lists, J equals to 0, a = length of each list
+              III. Traverse through each list and remove zeros, J++
+       
+    '''
     #check if the lists contain integer only if not throw exception
     for i in data:
         for j in i:
@@ -22,12 +39,20 @@ def make_jagged(data):
             j = j+1
 
 def sort_jagged(data):
+    '''
+       The function sorts the list of lists such that the one with list length comes first by using bubble sort.
+       
+       Algorithms of bubble sort:
+                      I. Traverse through the list of lists
+                      II. Traverse the list of lists from 0 to length - i -1
+                      III. swap if length of list at j is greater  length of list at j+1
+    '''
     #bubble sort
     length = len(data)
     #traverse through the list
     for i in range(length):
         for j in range(0, length - i -1):
-            #traverse the list from 0 to n-i-1
+            #traverse the list from 0 to length-i-1
             #swap if the length element at j is greater than lenght of element at j+1
             #then go to next
             if len(data[j]) > len(data[j+1]):
@@ -43,4 +68,6 @@ if __name__ == '__main__':
 
     make_jagged(x)
     print(x) # [[15, 2, 8, 5, 3], [3, 3, 7], [9, 1, 16, 3], [], [5]]
+    sort_jagged(x)
+    print(x) # [[], [5], [3, 3, 7], [9, 1, 16, 3], 15, 2, 8, 5, 3]]
     
