56b1585c303ccff3ee705dfc292e50857e2a1637
mansurmansur <mansurmansur@trentu.ca>
2020-07-25 04:37:17 -0400

    This Program generates password for the user:

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+'''
+   Author: Mansur Mansur                          date: 25th July, 2020
+   
+   Name of program: password generator
+   
+   Description: This program generates a unique password every time, to help users have a very strong password. Programs ask the user to enter how long the password should be, how many characters, should it be both uppercase and lowercase letter, and should it include numbers nad special symbols.
+   
+'''
